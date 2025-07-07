5e83787dab6eeb298a7fe2bfb09b594c111277b0
hermannKonyar <er.konyar@gmail.com>
2025-07-07 13:39:48 +0300

    Fourth Commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const path = require('path');
 
+const path = require('path');
 const app = express();
+
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
 
